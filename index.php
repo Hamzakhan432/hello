4284ffbd0337438fcdd1fa2e635477c1e10dc0e7
Hamzakhan432 <53292183+Hamzakhan432@users.noreply.github.com>
2019-07-25 18:56:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+include("header.php");
+  ?>
+  <br>
+  <br>
+<center>
+<h1>LogIn</h1>
+<form action="code.php" method="post">
+<table>
+<tr><td>Email:</td><td><input class="form-control" type="email" name="email"></td></tr>
+<tr><td>Password:</td><td><input class="form-control" type="password" name="pass"></td></tr>
+</table>
+ <input type="submit" class="btn" 	value="submit">
+</form>
